75911aef879cfc418e0f1090e53c990f34d0e9c2
Park-seungg <parkis1101@gmail.com>
2025-07-09 00:27:02 +0900

    완료 체크 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState(["공부하기", "코딩하기", "운동하기"]);
+  const [todos, setTodos] = useState([
+    { text: "공부하기", completed: false },
+    { text: "코딩하기", completed: false },
+    { text: "운동하기", completed: false },
+  ]);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    setTodos([form.todo.value, ...todos]);
+    setTodos([{ text: form.todo.value, completed: false }, ...todos]);
+    form.reset();
   };
 
-  const removeTodo = (seletedIndex) => {
-    const filterTodos = todos.filter((todo, index) => index != seletedIndex);
-    setTodos(filterTodos);
+  const handleDelete = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const handleToggleComplete = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
   };
 
   return (
@@ -22,9 +34,14 @@ function App() {
       </form>
       <ul>
         {todos.map((todo, i) => (
-          <li key={i}>
-            {todo}
-            <button onClick={() => removeTodo(i)}>X</button>
+          <li key={i} className={todo.completed ? "line-through" : ""}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleComplete(i)}
+            />
+            {todo.text}
+            <button onClick={() => handleDelete(i)}>삭제</button>
           </li>
         ))}
       </ul>
