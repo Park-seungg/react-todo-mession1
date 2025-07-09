866c3fc5e56faa07c69cb28f5b3c246f5d50f504
Park-seungg <parkis1101@gmail.com>
2025-07-09 20:55:05 +0900

    제목 수정

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
     <div className="min-h-screen w-full flex items-center justify-center bg-gray-100 m-0 p-0">
       <div className="max-w-md w-full p-6 bg-blue-100 rounded-xl shadow-md">
         <h1 className="text-3xl font-semibold text-center mb-6 text-indigo-600">
-          투두리스트
+          할 일 목록
         </h1>
         <TodoForm addTodo={addTodo} />
         <TodoList
